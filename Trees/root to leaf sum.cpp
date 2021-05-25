77030971a7e9494c6c8c47cc6dbee1650ce9c79e
Vipul Raj <57625616+VipulRaj-123@users.noreply.github.com>
2021-05-25 19:49:59 +0530

    Update root to leaf sum.cpp

--- Trees/root to leaf sum.cpp
+++ Trees/root to leaf sum.cpp
@@ -8,3 +8,41 @@ return val;
 return treepathsum(root->left,val)+treepathsum(root->right,val);
 }
                             
+               /*   Example 1:
+
+Input :      
+           6                               
+         /   \                          
+        3     5                      
+      /   \     \
+     2    5      4             
+        /  \                        
+       7    4  
+
+Output: 13997
+
+Explanation :
+There are 4 leaves, hence 4 root to leaf paths:
+Path                      Number
+6->3->2                   632
+6->3->5->7                6357
+6->3->5->4                6354
+6->5>4                    654   
+Answer = 632 + 6357 + 6354 + 654 = 13997 
+Example 2:
+
+Input :    
+           10                               
+         /   \                          
+        20     30                      
+      /   \     
+     40       60    
+
+Output :  2630
+
+Explanation:
+Path                      Number
+10->20->40                1240
+10->20->60                1260
+10->30                    130
+1240+1260+130 = 2630                 */
