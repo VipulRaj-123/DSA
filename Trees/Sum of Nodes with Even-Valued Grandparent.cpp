fd7efc8e1b47add9978e0b20beca35e1d3f3d808
Vipul Raj <57625616+VipulRaj-123@users.noreply.github.com>
2021-05-27 16:26:24 +0530

    Update Sum of Nodes with Even-Valued Grandparent.cpp

--- Trees/Sum of Nodes with Even-Valued Grandparent.cpp
+++ Trees/Sum of Nodes with Even-Valued Grandparent.cpp
@@ -1,4 +1,13 @@
 /* Sum of Nodes with Even-Valued Grandparent */
+
+/*  Example 1:
+https://assets.leetcode.com/uploads/2019/07/24/1473_ex1.png
+  
+
+
+Input: root = [6,7,8,2,7,1,3,9,null,1,4,null,null,null,5]
+Output: 18
+Explanation: The red nodes are the nodes with even-value grandparent while the blue nodes are the even-value grandparents. */
 class Solution {
 public:
     int sum=0;
