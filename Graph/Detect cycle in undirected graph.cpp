1ff98e4ef880b5a4d2f91edf0a6d8974c28ff83e
Vipul Raj <57625616+VipulRaj-123@users.noreply.github.com>
2021-06-18 22:13:43 +0530

    Add files via upload

---
+++ Graph/Detect cycle in undirected graph.cpp
@@ -0,0 +1,38 @@
+/* Detect cycle in undirected graph using BFS */
+#include <bits/stdc++.h>
+#define pb push_back
+using namespace std;
+bool detectcycle(vector<int>adj[],int n){
+	int visited[n]={0};
+	queue<pair<int,int>>q;
+	for(int i=0;i<n;i++){
+	  if(!visited[i]){
+	  	visited[i]=1;
+	  	q.push({i,-1});
+	  	while(!q.empty()){
+	  		int node=q.front().first;
+	  		int parent=q.front().second;
+	  		for(int i=0;i<adj[node].size();i++){
+	  			if(!visited[i]){
+	  				visited[i]=1;
+	  				q.push({i,node});
+				  }
+				  else if(parent!=i)
+				   return false;
+			  }
+		  }
+	  }	
+	}
+	return true;
+}
+int main(){
+	int n,m,u,v;
+	cin>>n>>m;
+	vector<int>adj[n];
+	while(m--){
+		cin>>u>>v;
+		adj[u].pb(v);
+		adj[v].pb(u);
+	}
+	cout<<detectcycle(adj,n)<<endl;
+}
