e08fede6e87b7bec9a92a3aee94e88f6bb930c44
Vipul Raj <57625616+VipulRaj-123@users.noreply.github.com>
2021-07-08 22:06:28 +0530

    Add files via upload

---
+++ Graph/Maximum jump.cpp
@@ -0,0 +1,48 @@
+/*Given an array of non-negative integers arr, you are initially positioned at start index of the array. When you are at index i, you can jump to i + arr[i] or i - arr[i], check if you can reach to any index with value 0.
+
+Notice that you can not jump outside of the array at any time.
+
+Example 1:
+
+Input: arr = [4,2,3,0,3,1,2], start = 5
+Output: true
+Explanation: 
+All possible ways to reach at index 3 with value 0 are: 
+index 5 -> index 4 -> index 1 -> index 3 
+index 5 -> index 6 -> index 4 -> index 1 -> index 3 
+Example 2:
+
+Input: arr = [4,2,3,0,3,1,2], start = 0
+Output: true 
+Explanation: 
+One possible way to reach at index 3 with value 0 is: 
+index 0 -> index 4 -> index 1 -> index 3
+Example 3:
+
+Input: arr = [3,0,2,1,2], start = 2
+Output: false
+Explanation: There is no way to reach at index 1 with value 0. */
+bool canReach(vector<int>& arr, int start) {
+       vector<int>visited(arr.size(),0);
+        int n=arr.size();
+        queue<int>q;
+        visited[start]=1;
+        q.push(start);
+        while(!q.empty()){
+            int x=q.front();
+            q.pop();
+            if(arr[x]==0)
+                return true;
+            int u=x+arr[x];
+            int v=x-arr[x];
+            if(u>=0&&u<=n-1&&!visited[u]){
+                visited[u]=1;
+                q.push(u);
+            }
+            if(v>=0&&v<=n-1&&!visited[v]){
+                visited[v]=1;
+                q.push(v);
+            }
+        }
+        return false;
+    }
